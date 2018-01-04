500ad7e0597c4ee64f97045aa2acbdb9e17059f7
KOITT <KOITT@KOITT-PC22>
2018-01-04 13:06:39 +0900

    5장 프로그래밍연습 7번문제

--- ch05/src/practice/Account.java
+++ ch05/src/practice/Account.java
@@ -2,38 +2,46 @@ package practice;
 
 public class Account {
 
-	private String Owner;
+	private String owner;
 	private long balance;
-	
+
 	//6-1getter, setter 
 	public String getOwner() {
-		return Owner;
+		return owner;
 	}
-
-
 
 	public void setOwner(String owner) {
-		Owner = owner;
+		this.owner= owner;
 	}
-
-
 
 	public long getBalance() {
 		return balance;
 	}
 
-
-
 	public void setBalance(long balance) {
 		this.balance = balance;
 	}
-	
-	//6-2
-	public Account(string Owner, long balace) {
-		this.Owner = Owner;
-		this.balance = balance;
-		
-	}
-	
 
+	//6-2
+	public Account(String owner, long balance) {
+		this.owner = owner;
+		this.balance = balance;
+	}
+	//7
+	public long deposit(long amount) {
+		return this.balance += amount;
+	}
+
+	public long withdraw(long amount) {
+		return this.balance -= amount;
+	}
+
+	public static void main(String[] args) {
+		Account account = new Account("홍길동", 100000);
+		System.out.println("현재잔액: " + account.deposit(1500000));
+		System.out.println("현재잔액: " + account.withdraw(500000));
+	}
 }
+
+
+
